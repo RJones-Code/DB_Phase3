1a4e33ea577129c7ecfe03e8a9e327216e04f435
Russ <russj065@gmail.com>
2025-04-25 12:45:39 -0400

    added and edited files for signup

---
+++ app/src/main/java/com/example/Phase3/SignUpRequest.java
@@ -0,0 +1,25 @@
+package com.example.Phase3;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SignUpRequest extends StringRequest {
+    private final Map<String, String> params;
+
+    public SignUpRequest(String studentID, String email, String password, String url, Response.Listener<String> listener) {
+        super(Request.Method.POST, url, listener, null);
+        params = new HashMap<>();
+        params.put("sid", studentID);
+        params.put("email", email);
+        params.put("password", password);
+    }
+
+    @Override
+    protected Map<String, String> getParams() {
+        return params;
+    }
+}
